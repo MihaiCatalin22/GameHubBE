ce5faac686fca574c3c89877d12a1ddca4e76096
Catalin Mihai Popoiu <117656515+MihaiCatalin20@users.noreply.github.com>
2024-03-12 17:05:55 +0100

    Updated "ForumPost" class
    - added Category enum used for the ForumPost class
    - added Comment class used for the ForumPost class

--- src/main/java/com/Gamehub/backend/domain/Comment.java
+++ src/main/java/com/Gamehub/backend/domain/Comment.java
@@ -1,25 +1,21 @@
 package com.Gamehub.backend.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.*;
 import java.util.Date;
-import java.util.Set;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class ForumPost {
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String title;
 
     @Lob
     private String content;
@@ -30,13 +26,6 @@ public class ForumPost {
     @CreationTimestamp
     private Date creationDate;
 
-    @ManyToMany
-    private Set<User> likes;
-
-    @OneToMany(mappedBy = "forumPost", cascade = CascadeType.ALL)
-    private List<Comment> comments;
-
-    @Enumerated(EnumType.STRING)
-    private Category category;
+    @ManyToOne
+    private ForumPost forumPost;
 }
-
