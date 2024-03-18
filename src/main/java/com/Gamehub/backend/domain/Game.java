cc56456059861182c4a79236650e60c2bac85d12
Catalin Mihai Popoiu <117656515+MihaiCatalin20@users.noreply.github.com>
2024-03-18 19:50:24 +0100

    Updated Game class, created Genre enum

--- src/main/java/com/Gamehub/backend/domain/Game.java
+++ src/main/java/com/Gamehub/backend/domain/Game.java
@@ -1,11 +1,32 @@
 package com.Gamehub.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
+
+@Entity
+@Table(name = "games")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
+
+    @Lob
     private String description;
-    private String genre;
+
+    @Enumerated(EnumType.STRING)
+    private Genre genre;
+
+    @Temporal(TemporalType.DATE)
     private Date releaseDate;
+
     private String developer;
 }
