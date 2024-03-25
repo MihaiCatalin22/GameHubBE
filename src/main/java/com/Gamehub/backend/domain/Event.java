abec11781784075b89cceca45f74f915452d3c34
Catalin Mihai Popoiu <117656515+MihaiCatalin20@users.noreply.github.com>
2024-03-25 10:05:42 +0100

    Enhanced Event domain class

--- src/main/java/com/Gamehub/backend/domain/Event.java
+++ src/main/java/com/Gamehub/backend/domain/Event.java
@@ -1,12 +1,45 @@
 package com.Gamehub.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Lob
     private String description;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
     private Date startDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
     private Date endDate;
-    private Set<User> participants;
+
+    @ManyToMany
+    @JoinTable(
+            name = "event_participants",
+            joinColumns = @JoinColumn (name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> participants = new HashSet<>();
 }
